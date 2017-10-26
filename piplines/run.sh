c86a2471d0b9cd932e44766a3163356ba62857fe
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-10-26 19:48:36 +0800

    fix check cell pipeline

--- piplines/run.sh
+++ piplines/run.sh
@@ -9,6 +9,7 @@ batch=`expr $lines / 20`
 
 
 for i in {0..19}
+#i=0
 do
     let start=$i*$batch
     if [ $start = 0 ]; then
@@ -18,11 +19,15 @@ do
     if [ $i = 19 ]; then
 	end=$lines
     fi
+
+    echo "deleting old outputs"
     rm -rf outputs$i
     mkdir outputs$i
+    
     echo "regenerating file list $i"
     cellprofiler -p check_cells.cpproj --file-list $file -o outputs$i
     echo "starting task$i"
 
-    cellprofiler -p outputs$i/Batch_data.h5 -cr -f $start -l $end > outputs$i/log.log &
+    cellprofiler -p outputs$i/Batch_data.h5 -cr -f $start -l $end > outputs$i/log.log 2>&1 &
 done
+    
