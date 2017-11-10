cf6cc6e5a217df4d502bfe5534d961eec18406c6
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-11-10 21:48:32 +0800

    optimize code to parallel computing

--- pipeline/gather_outputs.py
+++ pipeline/gather_outputs.py
@@ -11,50 +11,61 @@ import sys
 import numpy
 sys.path.append("../")
 from TCGAMaxim.utils import printProgressBar
+import multiprocessing as mp
+from multiprocessing import Pool
+from multiprocessing import Value
+import time
+import threading
+
+thread_count = Value('d', 0)
+thread_total = Value('d', 0)
+thread_finished = Value('d', 0)
 
 id_matcher = re.compile("TCGA-\w{2}-\w{4}-\w{3}-\w{2}-\w{3}")
-def main(dirname, outputname):
+def main(dirname, outputname, task_pool = 20):
+    global thread_count
     for gathername, sourcename in [("cell.csv", "MyExpt_cell.csv"),
                                    ("cytoplasm.csv", "MyExpt_Cytoplasm.csv"),
                                    ("nulei.csv", "MyExpt_nulei.csv")]:
+        
         print("processing %s files" % sourcename)
         cellfiles = []
 
+        pool = Pool(task_pool)
+
         recurse_find(dirname, cellfiles, sourcename)
         all_data_matrix = {}
+
+        progress = {
+            'all': len(cellfiles),
+            'i': 0
+            }
+
+        def file_process_callback(_):
+            progress['i'] = progress['i'] + 1
+            printProgressBar(progress['i'], progress['all'],
+                             prefix=filename, length=50)
+
+        thread_count.value = 0
+        thread_total.value = 1
+        thread_finished.value = 0
+        for filename in cellfiles:
+            #file_process_callback(file_processor(filename))
+            pool.apply_async(file_processor, (filename, all_data_matrix, ),
+                             callback=file_process_callback)
+
+        while(thread_finished.value < task_pool):
+            printProgressBar(thread_count.value,
+                             thread_total.value,
+                             prefix=("%d/%d" % (thread_count.value, thread_total.value)),
+                             length=50)
+            time.sleep(0.1)
+            
+        pool.close()
+        pool.join()
+
         with open(gathername, "w") as f:
             writer = csv.writer(f)
-
-            i = 0
-            all = len(cellfiles)
-            for filename in cellfiles:
-                i=i+1
-                printProgressBar(i, all, prefix=filename, length=50)
-                features = file_processor(filename)
-
-                for row in features:
-                    if row[0] in all_data_matrix:
-                        all_data_matrix[row[0]] = numpy.append(
-                            all_data_matrix[row[0]],
-                            numpy.array([row[1:]]).astype(numpy.float),
-                            axis=0)
-
-                        # this will reduce memory use
-                        '''if all_data_matrix[row[0]].shape[0] == 10:
-                            matrix = all_data_matrix[row[0]]
-                            mean = numpy.mean(matrix, axis=0).tolist()
-                            std = numpy.std(matrix, axis=0).tolist()
-                            mean.extend(std)
-                            mean.insert(0, row[0])
-                            writer.writerow(mean)
-                            del all_data_matrix[row[0]]
-                        '''
-
-                    else:
-                        all_data_matrix[row[0]] = numpy.array(
-                            [row[1:]]).astype(numpy.float)
-                        #print(matrix)
-
             all = len(all_data_matrix)
             i = 0
             for id in all_data_matrix:
@@ -65,6 +76,7 @@ def main(dirname, outputname):
                 matrix = all_data_matrix[id]
                 if(len(matrix) < 10 ):
                     print("warn: %s not enough lines(%d)" % (id, len(matrix)))
+                    continue
 
                 numpy_matrix = matrix#numpy.array(matrix).astype(numpy.float)
                 mean = numpy.mean(numpy_matrix, axis=0)
@@ -74,27 +86,58 @@ def main(dirname, outputname):
                 mean_list.extend(std_list)
                 mean_list.insert(0, id)
                 writer.writerow(mean_list)
+                
 
-
-
-def file_processor(filename):
+def file_processor(filename, all_data_matrix):
     global id_matcher
-    
-    all_features = []
-    with open(filename, "r") as f:
+
+    line_count = 0
+    with open(filename, 'r') as f:
+        
         reader = csv.reader(f, delimiter=",")
-
-        reader.next()   # skip the first meta line
+        reader.next()
+        
         for line in reader:
-            id = id_matcher.search(line[2]).group()
+            line_count += 1
+            
+        with thread_total.get_lock():
+            thread_total.value += line_count
 
-            features = line[8:]
-            features.insert(0, id)
+    f = open(filename, 'r')
+    reader = csv.reader(f, delimiter = ',')
+    reader.next()
+    for line in reader:
 
-            all_features.append(features)
+        id = id_matcher.search(line[2]).group()
 
-    return all_features
+        numpy_line = numpy.array([line[8:]]).astype(numpy.float)
+        numpy_line[numpy_line == None] = 0
+        if id in all_data_matrix:
+            all_data_matrix[id] = numpy.append(
+                all_data_matrix[id],
+                numpy_line,
+                axis=0)
 
+            # this will reduce memory use
+            '''if all_data_matrix[row[0]].shape[0] == 10:
+            matrix = all_data_matrix[row[0]]
+            mean = numpy.mean(matrix, axis=0).tolist()
+            std = numpy.std(matrix, axis=0).tolist()
+            mean.extend(std)
+            mean.insert(0, row[0])
+            writer.writerow(mean)
+            del all_data_matrix[row[0]]
+            '''
+
+        else:
+            all_data_matrix[id] = numpy_line
+
+        with thread_count.get_lock():
+            thread_count.value += 1
+
+    with thread_finished.get_lock():
+        thread_finished.value += 1
+        #print(matrix)
 
 def recurse_find(dirname, results, contains):
     if os.path.isdir(dirname):
