6b7af43ace14c2131c389971f083ee54d48854c7
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-11-23 00:33:50 +0800

    fit python code requirement

--- img_picker.py
+++ img_picker.py
@@ -1,9 +1,9 @@
 '''
 This script picks the top 20 best histology image from the input
-directory to the output directory according to the image pixel 
+directory to the output directory according to the image pixel
 density. This is the preprocess of the massive images.
 
-This script use parallel couputing, task pool it set to 20 by 
+This script use parallel couputing, task pool it set to 20 by
 default. Change it according to your machine configuration.
 '''
 
@@ -17,48 +17,52 @@ from multiprocessing import Pool
 import time
 import sys
 
-def calc_density(image, threshold = 150):
-    pixel = image.load();
+
+def calc_density(image, threshold=150):
+    pixel = image.load()
 
     pixel_count = image.size[0] * image.size[1] * 3
     color_count = 0
     for i in range(image.size[0]):
         for j in range(image.size[1]):
             # transparent pixel don't count
-            if pixel[i,j][3] < 125:
+            if pixel[i, j][3] < 125:
                 continue
             # black pixel don't count
-            if pixel[i,j][0] + pixel[i,j][1] + pixel[i,j][2] < 50:
+            if pixel[i, j][0] + pixel[i, j][1] + pixel[i, j][2] < 50:
                 continue
             
-            if pixel[i,j][0] < threshold:
+            if pixel[i, j][0] < threshold:
                 color_count = color_count + 1
-            if pixel[i,j][1] < threshold:
+            if pixel[i, j][1] < threshold:
                 color_count = color_count + 1
-            if pixel[i,j][2] < threshold:
+            if pixel[i, j][2] < threshold:
                 color_count = color_count + 1
 
     return color_count / float(pixel_count)
 
+
 task_count = 0
 task_sum = 0
+
+
 def calc_task(folder, image_file):
 
     density = calc_density(Image.open(os.path.join(folder, image_file)))
 
     return (folder, image_file, density)
 
+
 def task_callback(result):
     import re
     global task_count, task_sum, patients, start_time
-
 
     folder, image_file, density = result
     id_matcher = re.compile("TCGA-\w{2}-\w{4}-\w{3}-\w{2}-\w{3}")
     id = id_matcher.search(image_file).group()
     
     if id in patients:
-        patients[id][image_file] =  density
+        patients[id][image_file] = density
     else:
         patients[id] = {image_file: density}
 
@@ -69,7 +73,7 @@ def task_callback(result):
     time_each = used_time / task_count
     left_time = time_each * (task_sum - task_count)
 
-    used_time_str = ("%d:%d" % (int(used_time / 60), int(used_time % 60)))
+    # used_time_str = ("%d:%d" % (int(used_time / 60), int(used_time % 60)))
     left_time_str = ("%02d:%02d:%02d" % (int(left_time / 3600),
                                          int((left_time % 3600) / 60),
                                          int(left_time % 60)))
@@ -82,7 +86,9 @@ def task_callback(result):
 
 patients = {}
 start_time = 0
-def main(folder, target_folder, task_pool = 20, tmpfilename = "image_metas.pkl"):
+
+
+def main(folder, target_folder, task_pool=20, tmpfilename="image_metas.pkl"):
     print("data file name: %s" % tmpfilename)
     if not os.path.isfile(tmpfilename):
         print "image density not exists, calculating"
@@ -94,18 +100,17 @@ def main(folder, target_folder, task_pool = 20, tmpfilename = "image_metas.pkl")
 
         task_count = 0
         pool = Pool(task_pool)
-        #i = 0
+        # i = 0
         start_time = time.time()
         for image_file in os.listdir(folder):
-            #i = i + 1
-            #if i==10:
+            # i = i + 1
+            # if i==10:
             #    break
-            #calc_task(os.path.join(folder, image_file))
+            # calc_task(os.path.join(folder, image_file))
             pool.apply_async(calc_task, (folder, image_file, ),
-                         callback=task_callback)
+                             callback=task_callback)
         pool.close()
         pool.join()
-        
 
         with open(tmpfilename, "wb") as output:
             pickle.dump(patients, output)
@@ -121,7 +126,7 @@ def main(folder, target_folder, task_pool = 20, tmpfilename = "image_metas.pkl")
         if not os.path.exists(os.path.join(target_folder, id)):
             os.mkdir(os.path.join(target_folder, id))
         for key, value in sorted(patients[id].iteritems(),
-                                 key=lambda (k,v): (v,k))[-20:]:
+                                 key=lambda (k, v): (v, k))[-20:]:
             shutil.copyfile(os.path.join(folder, key),
                             os.path.join(target_folder, id, key))
             copy_count = copy_count + 1
@@ -129,11 +134,16 @@ def main(folder, target_folder, task_pool = 20, tmpfilename = "image_metas.pkl")
 
     print "copy finished"
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        main("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-processed",
-             "/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-selected",
-             tmpfilename = sys.argv[1])
+        main("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/\
+svs-processed",
+             "/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/\
+svs-selected",
+             tmpfilename=sys.argv[1])
     else:
-        main("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-processed",
-             "/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-selected")
+        main("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/\
+svs-processed",
+             "/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/\
+svs-selected")
