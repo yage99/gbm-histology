e80ceb597563c173b2440990b1af5646464350b3
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-11-25 02:43:05 +0800

    add matlab codes

--- matlab/loaddata.m
+++ matlab/loaddata.m
@@ -2,17 +2,18 @@ function [histology_cell, histology_cytoplasm, histology_nulei,...
           clinical, data_expression, data_cna, data_mrna] = ...
         loaddata()
 
-histology_cell = readtable('../source/cell.csv');
+histology_cell = readtable('../source/cell.csv', 'TreatAsEmpty', 'NA');
 histology_cytoplasm = readtable('../source/cytoplasm.csv');
 histology_nulei = readtable('../source/nulei.csv');
 
 clinical = readtable('../source/clinical.csv');
 
-data_expression = ...
-    readtable('../source/data_expression_Zscores.txt');
+data_expression = readtable('../source/data_expression_Zscores.txt', ...
+                            'TreatAsEmpty', 'NA');
 % TODO: change to zscore
 data_cna = readtable('../source/data_linear_CNA.txt');
-data_mrna = readtable('../source/data_mRNA_median_Zscores.txt');
+data_mrna = readtable('../source/data_mRNA_median_Zscores.txt', ...
+                      'TreatAsEmpty', 'NA');
 
 keeps = true(height(clinical), 1);
 [keeps] = delnan(histology_cell, keeps);
@@ -23,11 +24,20 @@ keeps = true(height(clinical), 1);
 [keeps] = delnan(data_mrna, keeps);
 
 histology_cell = histology_cell(keeps, :);
+histology_cell{:,2:end} = normalizemeanstd(histology_cell{:,2:end});
 histology_cytoplasm = histology_cytoplasm(keeps, :);
+histology_cytoplasm{:,2:end} = ...
+    normalizemeanstd(histology_cytoplasm{:,2:end});
 histology_nulei = histology_nulei(keeps, :);
+histology_nulei{:,2:end} = ...
+    normalizemeanstd(histology_nulei{:,2:end});
 clinical = clinical(keeps, :);
 data_expression = data_expression(keeps, :);
+data_expression{:,2:end} = ...
+    normalizemeanstd(data_expression{:,2:end});
 data_cna = data_cna(keeps, :);
+data_cna{:,2:end} = normalizemeanstd(data_cna{:,2:end});
 data_mrna = data_mrna(keeps, :);
+data_mrna{:,2:end} = normalizemeanstd(data_mrna{:, 2:end});
 
 end
--- matlab/main.m
+++ matlab/main.m
@@ -1,3 +1,7 @@
+addpath(genpath('~/Documents/MATLAB/tools/'))
+
 [histology_cell, histology_cytoplasm, histology_nulei, ...
  clinical, data_expression, data_cna, data_mrna] = loaddata();
+
+
 
