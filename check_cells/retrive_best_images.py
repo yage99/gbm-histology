dc64ff0874b8a01317b180a67ed409cb94d16b7f
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-11-30 19:17:43 +0800

    some repairs

--- check_cells/retrive_best_images.py
+++ check_cells/retrive_best_images.py
@@ -1,6 +1,6 @@
 '''
-This script picks images by the number of nuleis recognized in 
-cellprofiler. 
+This script picks images by the number of nuleis recognized in
+cellprofiler.
 '''
 
 
@@ -10,7 +10,9 @@ import shutil
 import re
 
 id_matcher = re.compile("TCGA-\w{2}-\w{4}-\w{3}-\w{2}-\w{3}")
-def main(filename):
+
+
+def read_info(filename):
     global id_matcher
     with open(filename, "r") as f:
         reader = csv.reader(f, delimiter=',')
@@ -31,19 +33,27 @@ def main(filename):
             all_infos[id][image_name] = number_of_nuclei
             
         return all_infos
-            
 
-if __name__ == "__main__":
 
-    all_infos = main("./check_cells/all.csv")
+def main():
+    all_infos = {}
+    for i in range(20):
+        file = "outputs%d/.csv" % i
+        print 'reading file %s' % file
+        all_infos.extend(read_info(file))
 
-    i = 0
-    all = len(all_infos) * 10
+    print "copying files"
+
     for id in all_infos:
         for key, value in sorted(all_infos[id].iteritems(),
-                                 key=lambda (k, v): (v, k) )[-10:]:
+                                 key=lambda (k, v): (v, k))[-10:]:
             parent_folder = id_matcher.search(key).group()
-            source = os.path.join("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-selected", parent_folder, key)
-            target = os.path.join("/media/af214dbe-b6fa-4f5e-932a-14b133ba4766/zhangya/svs-best", key)
+            source = os.path.join("/media/af214dbe-b6fa-4f5e\
+-932a-14b133ba4766/zhangya/svs-selected", parent_folder, key)
+            target = os.path.join("/media/af214dbe-b6fa-4f5e\
+-932a-14b133ba4766/zhangya/svs-best", key)
             shutil.copy(source, target)
-            
+
+
+if __name__ == "__main__":
+    main()
