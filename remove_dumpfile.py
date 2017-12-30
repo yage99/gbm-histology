98c7db1dfaa52b2f47009dd4fd028259749843f5
Ya Zhang <zhangya@mail.ustc.edu.cn>
2017-12-31 00:43:41 +0800

    optimize

--- remove_dumpfile.py
+++ remove_dumpfile.py
@@ -4,30 +4,22 @@ from TCGAMaxim.utils import printProgressBar
 
 
 def main(source, folders):
-    source_list = sorted(
-            sp.check_output(['find', source, '-name', '*.png']).split('\n'))
-    source_list = map(lambda(f): os.path.basename(f), source_list)
     dir_list = []
     for dir in folders:
-        _list = sorted(
-            sp.check_output(['find', dir, '-name', '*.png']).split('\n'))
+        _list = sp.check_output(['find', dir, '-name', '*.png']).split('\n')
         _list = map(lambda(f): os.path.basename(f), _list)
         dir_list.append(_list)
 
-    to_delete = []
-    for file_name in source_list:
-        for file_list in dir_list:
-            if file_name in file_list:
-                to_delete.append[file_name]
-                file_list.remove(file_name)
-                break
+    count = 0
+    for file_list in dir_list:
+        count += len(file_list)
 
-    count = len(to_delete)
     i = 0
-    for file in to_delete:
-        sp.call(['rm', os.path.join(source, file)])
-        i += 1
-        printProgressBar(i, count)
+    for file_list in dir_list:
+        for file in file_list:
+            sp.call(['rm', os.path.join(source, file)])
+            i += 1
+            printProgressBar(i, count)
 
 
 if __name__ == "__main__":
